de40801a498b3f5aae8abdaac323dc424a39cfae
Askat Iliasbekov <97965896+Askatice@users.noreply.github.com>
2022-04-30 00:33:16 +0600

    Initial commit

---
+++ app/src/main/java/com/ssa/taskapp/ui/profile/ProfileFragment.java
@@ -0,0 +1,95 @@
+package com.ssa.taskapp.ui.profile;
+
+
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.ActivityResultLauncherKt;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.bumptech.glide.Glide;
+import com.ssa.taskapp.databinding.FragmentProfileBinding;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ProfileViewModel profileViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textNotifications;
+        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initListener();
+        getImages();
+    }
+
+    private void initListener() {
+       binding.addImage.setOnClickListener(view -> {
+           Intent intent = new Intent(Intent.ACTION_PICK);
+           intent.setType("image/*");
+           activityResultLauncher.launch(intent);
+
+       });
+    }
+
+    public void getImages() {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK) {
+                try {
+                    final Uri imageUri = result.getData().getData();
+                    final InputStream imageStream = requireActivity().getContentResolver().openInputStream(imageUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    binding.imgProfile.setImageBitmap(selectedImage);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(requireActivity(), "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+            }else {
+                Toast.makeText(requireActivity(), "You haven't picked Image", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
